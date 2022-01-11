f201500528aef7a2ae9217e6d1dc637e7bf101c7
Nithya Narayanan <n.nithya@pm.me>
2022-01-11 13:41:37 -0800

    Completed FollowerList.

--- src/components/FollowerList.js
+++ src/components/FollowerList.js
@@ -1,2 +1,15 @@
 import React from 'react';
+import Follower from './components/Follower.js';
 
+class FollowerList extends React.Component{
+
+    render(){
+        return(<div>
+            {this.props.followers.map( user =>{
+                return(<Follower img={user.avatar_url} name={user.login} key={user.id}/>)
+            })}
+        </div>)
+    }
+}
+
+export default FollowerList;
